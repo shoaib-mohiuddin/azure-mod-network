7372b5a03a82e2a9e27c798680fdb0ad8da953a3
shoaib-mohiuddin <shoaib.mohiuddin@cloudreach.com>
2023-03-30 19:54:55 +0530

    runs pre-commit

--- examples/networking/main.tf
+++ examples/networking/main.tf
@@ -2,7 +2,7 @@ module "main" {
   source = "../.."
   #source = "git::https://github.com/shoaib-mohiuddin/azure-mod-network"
 
-  vnet_resource_group_name = var.name
+  vnet_resource_group_name = "${var.name}-rg"
   location                 = "North Europe"
   address_space            = ["192.168.0.0/23"]
   subnet_address_prefixes  = ["192.168.0.0/27"]
